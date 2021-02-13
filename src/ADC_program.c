aee4705f51c1f650fa85a88bc1f3c1246e316be7
yousef <36451974+yousefabdelbadea@users.noreply.github.com>
2021-02-13 16:21:20 +0200

    Add files via upload

---
+++ src/ADC_program.c
@@ -0,0 +1,83 @@
+/*
+ * ADC_program.c
+ *
+ *  Created on: Jan 17, 2021
+ *      Author: Yousef
+ */
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "DIO_interface.h"
+#include "RCC_interface.h"
+
+#include "ADC_private.h"
+#include "ADC_interface.h"
+#include "ADC_config.h"
+void ADC_VoidInit(void)
+{
+	CLR_REG(ADC_CR1);
+	
+	/*for enable and disable scan mode*/
+	if(SCAN_MODE == ENABLE_SCAN_MODE)
+		SET_BIT(ADC_CR1,SCAN);
+	else 
+		CLR_BIT(ADC_CR1,SCAN);
+	
+	
+	CLR_REG(ADC_CR2);	
+	/*to data aligenment*/
+if(DATA_ALIGENMENT == RIGHT_ALIGNMENT)
+	CLR_BIT(ADC_CR2,ALIGN);
+else
+	SET_BIT(ADC_CR2,ALIGN);
+
+if (CONVERSION_MODE == CONTINOUS_CONVERSION)	
+	SET_BIT(ADC_CR2,CONT);
+else
+	CLR_BIT(ADC_CR2,CONT);
+	
+	CLR_REG(ADC_SMPR2);
+	/* Sampling time*/
+	ADC_SMPR2 |= SAMPLING_TIME;
+	
+	/*number of conversion = 1*/
+  ADC_SQR1 |= (NUMBER_CONVERSION << 20 );
+
+	  /*Channel_0 Rank_1*/
+	   ADC_SQR3 |= SELECTED_CHANNEL;
+		 
+	/*ADC_Power_On*/
+	SET_BIT(ADC_CR2,ADON);
+
+
+	/*reset calibration*/
+	SET_BIT(ADC_CR2,RCAL);
+
+	/*calibration*/
+	SET_BIT(ADC_CR2,CAL);
+	
+}
+
+void start_coversion(void)
+{
+	/*ADC_Power_On*/
+	SET_BIT(ADC_CR2,ADON);
+}
+
+u32 Get_Temp()
+{
+u32 x;
+	 //x = ((ADC_DR) * (0.120879));
+x = ((ADC_DR) * (0.122));
+
+return x;
+}
+
+f32 Get_Current()
+{
+
+f32 y = (ADC_DR - 409)/109.0;
+return y;
+}
+
--- src/main.c
+++ src/main.c
@@ -1,23 +1,91 @@
+#include "RCC_interface.h"
+#include "DIO_interface.h"
+#include "ADC_interface.h"
+#include "LCD.h"
+#include "SYSTICK_INTERFACE.h"
+#include "MULTIPLEXER_interface.h"
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
 
-#include"RCC_interface.h"
-#include"DIO_interface.h"
-#include"SYSTICK_INTERFACE.h"
+u32 temp_sensors[6] ;
+u32 current_senssors[3];
+u32 n;
 
+void Data_voidAcqusition(void);
 
 int main()
 {
 	RCC_voidInitSysClock();
+	/*Generate clock for GPPIOA*/
 	RCC_voidEnableClock(RCC_APB2 , 2);
-	MSTK_VidInit();
-	MGPIO_VidSetPinDirection  ( GPIOA , PIN1 , OUTPUT_SPEED_2MHZ_PP  );
+	/*Set direction of GPIOA pin 0 as input analog*/
+	MGPIO_VidSetPinDirection( GPIOA , PIN0 , INPUT_ANLOG );
+	/*Generate clock for GPPIOB*/
+	RCC_voidEnableClock(RCC_APB2 , 3);
+	/*Generate clock for ADC1*/
+	RCC_voidEnableClock(RCC_APB2 , 9);
+	/*initalizing ADC_1*/
+	ADC_VoidInit();
+	/*start coversion of ADC_1*/
+	start_coversion();
+	/*intialize LCD*/
+	LCD_init();
+	/*initalize Multiplexer */
+	HMultiplexer_voidInit();
+
+	for(u32 n =0 ; n<500000 ; n++){}
 	while(1)
 	{
-		MGPIO_VidSetPinValue ( GPIOA , PIN1 , HIGH );
-		_delay_ms(2000);
-	    MGPIO_VidSetPinValue ( GPIOA , PIN1 , LOW );
-	    _delay_ms(2000);
-	}
+		Data_voidAcqusition();
 
+		for( n =0 ; n<500000 ; n++){}
+	}
+	return 1;
 }
 
+void Data_voidAcqusition(void)
+{
 
+	u8 i,k;
+	u8 counter = 0;
+	for(i=0 ; i<6 ;i++)
+	{
+		HChannelSelection_void(counter);
+		for( n =0 ; n<100 ; n++){}
+		temp_sensors[i] = Get_Temp();
+		LCD_displayString("TEMP_");
+		LCD_intgerToString(i+1);
+		LCD_displayString(": ");
+		LCD_intgerToString(temp_sensors[i]);
+		for( n =0 ; n<500000 ; n++){}
+		LCD_clearScreen();
+		counter++;
+	}
+
+	for( k=0 ; k<3 ;k++)
+	{
+		HChannelSelection_void(counter);
+		for( n =0 ; n<100 ; n++){}
+		current_senssors[k] = Get_Current();
+		LCD_displayString("Current_");
+		LCD_intgerToString(k+1);
+		LCD_displayString(": ");
+		LCD_intgerToString(current_senssors[k]);
+		for( n =0 ; n<500000 ; n++){}
+		LCD_clearScreen();
+		counter++;
+	}
+	counter = 0;
+
+
+	/*
+	HChannelSelection_void(8);
+	current_senssors[0] = Get_Current();
+	LCD_displayString("Current_");
+	LCD_intgerToString(1);
+	LCD_displayString(": ");
+	LCD_intgerToString(current_senssors[0]);
+	for( n =0 ; n<500000 ; n++){}
+	LCD_clearScreen();
+*/
+}
