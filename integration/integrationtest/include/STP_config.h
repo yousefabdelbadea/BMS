f4ed2548f698b7598a81cf3d59b194cfdc95ce1b
Mohamed hamada <57867651+MohamedHamadaMohamed@users.noreply.github.com>
2021-07-11 16:29:10 +0200

    Update STP_config.h

--- integration/integrationtest/include/STP_config.h
+++ integration/integrationtest/include/STP_config.h
@@ -12,14 +12,14 @@
  #ifndef STP_CONFIG_H_
  #define STP_CONFIG_H_
 
- #define STP_SERIAL_DATA_PIN          PIN0
- #define STP_SERIAL_DATA_PORT         GPIOA
+ #define STP_SERIAL_DATA_PIN          PIN7
+ #define STP_SERIAL_DATA_PORT         GPIOB
  
- #define STP_STORE_CLOCK_PIN          PIN1
- #define STP_STORE_CLOCK_PORT         GPIOA
+ #define STP_STORE_CLOCK_PIN          PIN8
+ #define STP_STORE_CLOCK_PORT         GPIOB
  
- #define STP_SHIFT_CLOCK_PIN          PIN2
- #define STP_SHIFT_CLOCK_PORT         GPIOA
+ #define STP_SHIFT_CLOCK_PIN          PIN9
+ #define STP_SHIFT_CLOCK_PORT         GPIOB
 
  
  #endif
